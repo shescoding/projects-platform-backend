207680722f5167bb581d51746ad7bddd4a4b38a4
Nurmerey Shakhanova <shakhanova.n@gmail.com>
2019-12-16 12:02:20 -0800

    Removing secret key

--- projectsplatform/settings.py
+++ projectsplatform/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dulwepf_b#@eh19w)==v+pxvw$u$2gbxpwoevmzg)h-pl1mded'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
