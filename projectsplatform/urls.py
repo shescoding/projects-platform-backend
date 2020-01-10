f9e0d5400d91b742e9dca494f4a0bc1ac6777aca
Nurmerey Shakhanova <shakhanova.n@gmail.com>
2020-01-10 09:50:00 -0800

    URL patterns

--- projectsplatform/urls.py
+++ projectsplatform/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from scprojects import views
 
 urlpatterns = [
+    path('scprojects/', include('scprojects.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index, name='index')
 ]
