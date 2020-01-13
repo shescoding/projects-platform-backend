4a1c5245e2a088ac19bb43ffc61f3e8b13fc9040
Nurmerey Shakhanova <shakhanova.n@gmail.com>
2020-01-13 10:17:20 -0800

    First draft Project API

--- scprojects/views.py
+++ scprojects/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
+from django.core import serializers
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def projects(request):
+    projects = Project.objects.all()
+    data = serializers.serialize("json", projects)
+    return HttpResponse(data)
+
+
+# next up TODO's
+# add project
+# add user
+# login
