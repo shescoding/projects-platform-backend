b9c2baac2e92ec07d3e10dfa5b9156ff3422452e
Nev <nevena.stoeva@gmail.com>
2020-02-03 18:32:15 -0800

    Nullable Position
    Courtesy of Rey :)

--- scprojects/models.py
+++ scprojects/models.py
@@ -6,7 +6,7 @@ from django.contrib.postgres.fields import ArrayField
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     experience_lvl = models.PositiveSmallIntegerField(blank=True)
-    position = models.CharField(max_length=255, blank=True)
+    position = models.CharField(max_length=255, blank=True, null=True)
     is_active = models.BooleanField()
     github_username = models.CharField(max_length=255, blank=True)
     github_id = models.PositiveIntegerField(blank=True)
