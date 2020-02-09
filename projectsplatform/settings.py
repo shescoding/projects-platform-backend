6bbefe5b25da6f31115b5e43a43eb32cd24fc89e
Nurmerey Shakhanova <shakhanova.n@gmail.com>
2020-02-09 14:20:17 -0800

    Remove import secrets

--- projectsplatform/settings.py
+++ projectsplatform/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from dotenv import load_dotenv
 from pathlib import Path
-from .secrets import *
 
 env_path = str(Path('.') / '.env')
 load_dotenv(dotenv_path=env_path)
